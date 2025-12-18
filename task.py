b4708c206259c5c5d712cccaea4176791a38b006
dancaseb <danielisreeves@gmail.com>
2025-12-18 20:43:32 +0100

    calculate mid points automatically

--- task.py
+++ task.py
@@ -4,13 +4,19 @@ w_hat = np.array([0.1259391805448/2, 0.1259391805448/2, 0.1259391805448/2,0.1323
 A_hat = np.array([[0.1012865073235, 0.1012865073235], [0.7974269853531, 0.1012865073235], [0.1012865073235, 0.7974269853531], [0.4701420641051, 0.0597158717898],[0.4701420641051, 0.4701420641051],
 [0.0597158717898, 0.4701420641051], [1/3, 1/3]])
 
-N_start = np.array([[0,0], [3,0], [2,2]])
-N = np.array([[0,0], [3,0], [2,2], [2.5,1], [1,1], [1.5,0]])
 
+# User defined parameters
+N_start = np.array([[0,0], [3,0], [2,2]])
+N = np.zeros((6,2))
+N[0:3,:] = N_start
+for i in range(3):
+    N[3+i,:] = 0.5*(N_start[(i+1)%3,:] + N_start[(i+2)%3,:])
+# N = np.array([[0,0], [3,0], [2,2], [2.5,1], [1,1], [1.5,0]])
+print("N:", N)
 l = 0.5 # lambda
 mu = 0.3
 
-
+# rows == number of quadrature points, cols == basis functions
 def basic_func_P2(A):
     e0 = 1-A[:,0]-A[:,1]
     return np.column_stack([e0*(2*e0-1), A[:,0]*(2*A[:,0]-1), A[:,1]*(2*A[:,1]-1), 4*A[:,0]*A[:,1], 4*e0*A[:,1], 4*e0*A[:,0]])
@@ -32,14 +38,16 @@ dphi_de2 = dbasic_func_de2(A_hat)
 
 chi_local = np.dot(phi_A_hat, N) # shape (7,2), mapping to final element coordinates
 
+# This is redundant in the case of a affine mapping (triangle without curved edges),
+# J is constant for every quadrature point. 
 J = np.zeros((phi_A_hat.shape[0], 2, 2))  # shape (7,2,2)
 for j in range(phi_A_hat.shape[0]):
     J[j,:,:] = np.array([[np.dot(dphi_de1[j,:], N[:,0]), np.dot(dphi_de2[j,:], N[:,0])],
                         [np.dot(dphi_de1[j,:], N[:,1]), np.dot(dphi_de2[j,:], N[:,1])]])
 
 det_J = J[:,0,0]*J[:,1,1] - J[:,0,1]*J[:,1,0]
+# weights adjusted for final element
 w = w_hat * det_J
-
 
 J_inv = np.full_like(J, np.nan)
 for j in range(J.shape[0]):
@@ -48,7 +56,8 @@ for j in range(J.shape[0]):
     except np.linalg.LinAlgError:
         J_inv[j] = np.nan
 
-#is this correct?
+# compute derivates of basis functions with respect to x_1 and x_2
+# rows == quadrature points, cols == basis functions
 phi_dx1 = np.zeros_like(dphi_de1)
 for q in range(dphi_de1.shape[0]):
     phi_dx1[q,:] = J_inv[q,0,0]*dphi_de1[q,:] + J_inv[q,0,1]*dphi_de2[q,:]
@@ -58,8 +67,8 @@ phi_dx2 = np.zeros_like(dphi_de2)
 for q in range(dphi_de2.shape[0]):
     phi_dx2[q,:] = J_inv[q,1,0]*dphi_de1[q,:] + J_inv[q,1,1]*dphi_de2[q,:]
 
+# construct B matrix for each quadrature point
 B = np.zeros((phi_dx1.shape[0],3, 2*N.shape[0]))
-
 for q in range(phi_dx1.shape[0]):
     for a in range(N.shape[0]):
         B[q, 0, 2*a]   = phi_dx1[q,a]
@@ -77,4 +86,4 @@ for q in range(B.shape[0]):
     B_q = B[q,:,:]
     K_T += w[q] * np.dot(B_q.T, np.dot(C, B_q))
 
-print(K_T)
+# print(K_T)
